0abba9381df7309860b94dfb44284e8d83749304
tofekkhan <tofekkhan@localhost>
2022-02-08 00:29:30 -0600

    first commit

---
+++ address-service/src/test/java/com/springcloud/address/AddressServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springcloud.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("First line of code");
+	}
+
+}
